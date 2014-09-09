1b0013d3cda3e7cc807113b3d89b1375b5f99b20
stephen <stephen@moogsoft.com>
2014-09-09 16:28:34 +0100

    Updates with path

--- routes/editor.js
+++ routes/editor.js
@@ -23,22 +23,12 @@ router.get('/', function(req, res) {
         return;
     }
     //console.log(editFile);
-    //editFile = editFile.replace(/\\n/g, '%0A'); //Escape the escapes
-    //editFile = editFile.replace(/\\r/g, '%0D'); //Will need to convert them back before save
-    //editFile = editFile.replace(/\n/g,'\\n'); //Create newlines in a single text string
-    //editFile = editFile.replace(/\t/g,'\\t'); //Create newlines in a single text string
-    //editFile = editFile.replace(/\\/g,'\\\\'); //Double escape
-    //console.log(editFile);
-    //editFile = encodeURIComponent(editFile);
     res.render('editor', { title: 'File Editor '+fileName, editFile: editFile });
 });
 
 router.post('/', function(req, res, next) {
     var saveFile = req.body.postFile;
     console.log('Got Data');
-    saveFile.replace(/\r\n|\r|\n/g,'\n');
-    saveFile = saveFile.replace(/%0A/g, '\\n'); //Recover the escapes
-    saveFile = saveFile.replace(/%0D/g, '\\r'); //Convert them back before save
     console.log(saveFile);
     try {
         fs.writeFileSync(fileName,saveFile);
